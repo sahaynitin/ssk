9ec26f9c73bf64bc9d07dc2e86bdddb54ca2b49f
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-02 06:18:30 +0530

    Update youtube_callback_data.py

--- plugins/youtube_callback_data.py
+++ plugins/youtube_callback_data.py
@@ -79,7 +79,7 @@ async def catch_youtube_dldata(c, q):
     # await q.edit_message_reply_markup([[InlineKeyboardButton("Processing..")]])
 
     audio_command = [
-        "youtube-dl",
+        "yt-dlp",
         "-c",
         "--prefer-ffmpeg",
         "--extract-audio",
@@ -91,7 +91,7 @@ async def catch_youtube_dldata(c, q):
     ]
 
     video_command = [
-        "youtube-dl",
+        "yt-dlp",
         "-c",
         "--embed-subs",
         "-f", f"{format_id}+bestaudio",
